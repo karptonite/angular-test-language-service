478748d304c14ed5e89138227dc6f0b20b81cc80
Daniel Karp <danielkarp@gmail.com>
2017-06-14 15:39:45 -0400

    Example of angular language service error for absolute paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { TestService } from '@app/test.service';
+// If I use a relative path, below, I don't get a language service error
+// import { TestService } from './test.service';
 
+// Error:(7, 2) Angular: Can't resolve all parameters for AppComponent in /Users/karp/Documents/git/angular-test-language-service/src/app/app.component.ts: (?).
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor( private testService: TestService ) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestService } from '@app/test.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+     TestService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
